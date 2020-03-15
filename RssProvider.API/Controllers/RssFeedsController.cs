df465bf517837bb3674b7264341042006a63d66e
Bilal Taherkheli <bilal76@gmail.com>
2020-03-15 16:56:02 +0100

    api/feeds: controller sends some dummy rss feeds back in JSON format

---
+++ RssProvider.API/Controllers/RssFeedsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RssProvider.API.Controllers
+{
+  [ApiController]
+  public class RssFeedsController : ControllerBase
+  {
+    [HttpGet("api/feeds")]
+    public JsonResult GetRssFeeds()
+    {
+      return new JsonResult(
+        new List<object>()
+        {
+          new { id = 1, title = "feed 1", DateTime.UtcNow },
+          new { id = 2, title = "feed 453", DateTime.UtcNow }
+        });
+    }
+  }
+}
